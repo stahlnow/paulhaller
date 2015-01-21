bebbf774962f7625df0bfcfb444da84a136ab86f
stahl <office@stahlnow.com>
2015-01-21 23:32:57 +0100

    fixed urls.py

--- website/project/urls.py
+++ website/project/urls.py
@@ -1,12 +1,24 @@
-from django.conf.urls import patterns, include
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from django.conf import settings
-from django.conf.urls.static import static
 
 
-urlpatterns = patterns('',
-                       (r'^', include('website.urls')),
-                       (r'^admin/', include(admin.site.urls)),
-                       (r'^ckeditor/', include('ckeditor.urls')),
-                       ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = patterns(
+    '',
+    (r'^', include('website.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^ckeditor/', include('ckeditor.urls')),
+    )
+
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+            }),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+            }),
+        )
